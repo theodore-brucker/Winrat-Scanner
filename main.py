f8599d06fbb61bbffce748911b8c7241bdffad32
theodore-brucker <76602421+theodore-brucker@users.noreply.github.com>
2022-10-25 13:37:42 -0400

    refactoring

--- main.py
+++ main.py
@@ -4,11 +4,48 @@ from target import Target
 #Main 
 def main():
     os.system('cls')
-    targetIP = input("IP: ")
-    targetPorts = input("Ports separated by ',': ").replace(" ", "").split(",")
+    targetIP, *targetPorts = input("Input ip and ports separated by a space, ex (ip port1 port2 port3): ").split(" ")
     target = Target(targetIP, targetPorts)
-    print()
-    target.run()
+    run(target)
+
+commandArgNums = {
+    "setip": (1, 1),
+    "addports": (1, None),
+    "ping": (0, 0),
+    "ssh": (0, 1),
+    "quit": (0, 0),
+}
+
+def checkArgnum(command, args):
+    minArgs, maxArgs = commandArgNums[command]
+    if maxArgs is None:
+        return minArgs <= len(args)
+    return minArgs <= len(args) <= maxArgs
+
+def run(target):
+    while True:
+        print("------------------------------------------")
+
+        hello = input("Type a command to run: ").split(" ")
+        command, args = hello[0], hello[1:]
+        
+        if not checkArgnum(command, args):
+            print("Unexpected number of arguments")
+            continue
+
+        if command == "setip":
+            target.setIP(args[0])
+        if command == "addports":
+            target.addPorts(*args)
+        if command == "ping":
+            target.pingConnectivity()
+        elif command == "ssh":
+            if len(args) == 1:
+                target.checkSSH(args[0])
+            else:
+                target.checkSSH()
+        elif command == "quit":
+            quit()
 
 #Calls main when main.py is executed
 if __name__ == "__main__":
--- target.py
+++ target.py
@@ -1,30 +1,40 @@
 import os
+import paramiko
+from setuptools import Command
 
 #Class for the Target
 class Target:
 
     #Constructor with self and an ip
-    def __init__(self, ip):
-        self.ip = ip
-
-    #Constructor with self, an ip, and ports
-    def __init__(self, ip, ports):
-        self.ip = ip
+    def __init__(self, ip, ports=[]):
+        self.setIP(ip)
         self.ports = ports
 
+    def setIP(self, ip):
+        self.ip = ip
+
+    def addPorts(self, *ports):
+        self.ports.extends(ports)
+
     #Pings using ICMP protocol, does not use a port
-    def ping(self):
+    def pingConnectivity(self):
         response = os.popen(f"ping {self.ip}").read()
         print(response)
         if "Received = 4" in response:
             print(f"Ping to {self.ip} successful")
 
-    #Run self
-    def run(self):
-        while True:
-            print("------------------------------------------")
-            command = input("Type a command to run: ").lower()
-            if command == "ping":
-                self.ping()
-            elif command == "quit":
-                quit()
+    #SSH login to port 22
+    def checkSSH(self, port=22):
+        # self.ports = input("Port ranges (separated by ,): ") 
+        ssh_client = paramiko.SSHClient()
+        try:
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh_client.connect('10.3.32.32', port=22, username='amumu', password='amumu')
+        except paramiko.SSHException:
+            print("Login attempt failed")
+            return
+        stdin,stdout,stderr=ssh_client.exec_command('ls')
+        output = stdout.readlines()
+        for item in output:
+            print(item)
+
